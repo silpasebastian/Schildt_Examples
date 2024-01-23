74cba8fdf5844b0b9393131be73f6b5d124a33b1
Silpa S Sebastian <silpasebastian@yahoo.com>
2024-01-23 21:33:26 +0530

    First commit

---
+++ chapter_12/src/chapter_12/Wrap.java
@@ -0,0 +1,12 @@
+package chapter_12;
+
+public class Wrap {
+
+	public static void main(String[] args) {
+		Integer iob = Integer.valueOf(100);
+		int i = iob.intValue();
+		System.out.println(i+" "+iob);
+
+	}
+
+}
---
+++ chapter_12/src/chapter_12/autobox.java
@@ -0,0 +1,12 @@
+package chapter_12;
+
+public class autobox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer iob = 100;
+		int i = iob;
+		System.out.println(i+ " "+iob);
+	}
+
+}
